ae8da10e9a6bca25140a000117a7123f98e40c02
Michael Brewer <brewermichael208@gmail.com>
2021-01-02 12:03:00 -0700

    added style.css file with no code in it yet

---
+++ code.js
@@ -0,0 +1,35 @@
+function tree(){
+    for ( i = 0; i < 9; i ++){
+       if ( i == 0){
+        document.getElementById("line0").innerHTML = "        #";
+        console.log(document.getElementById("line0"));
+       }   
+        if (i ==1){
+        document.getElementById("line1").innerHTML = "       ###";
+       }
+        if (i ==2){
+        document.getElementById("line2").innerHTML = "      #####";
+       }
+        if (i ==3){
+        document.getElementById("line3").innerHTML = "     #######";
+       }
+       if (i ==4){
+        document.getElementById("line4").innerHTML = "    #########";
+       }
+       if (i ==5){
+        document.getElementById("line5").innerHTML = "   ###########";
+       }
+       if (i ==6){
+        document.getElementById("line6").innerHTML = "  #############";
+       }
+       if (i ==7){
+        document.getElementById("line7").innerHTML = " ###############";
+       }
+       if (i ==8){
+        document.getElementById("line8").innerHTML = "#################";
+       }
+    
+    }
+    
+}
+     
